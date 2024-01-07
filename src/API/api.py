baa85a70cab1fe3348a9e7d0891def9a7f4a7838
Ben-Edwards44 <benjedwards007@icloud.com>
2024-01-07 10:57:08 +0000

    Wrote the Python side of the json interface

--- src/API/api.py
+++ src/API/api.py
@@ -1,1 +1,37 @@
-#TODO: interface with go through json
+import json
+
+
+def str_to_list(string):
+    string = string[2:-2].split("], [")
+    
+    list = []
+    for i in string:
+        nums = i.split(", ")
+        nums = [int(x) for x in nums]
+
+        list.append(nums)
+
+    return list
+
+
+def load_board_state():
+    with open("interface.json", "r") as file:
+        data = file.read()
+
+    #board_dict in form {"board" : "[[...], [...], ...]"}
+    board_dict = json.loads(data)
+    board_str = board_dict["board"]
+
+    board = str_to_list(board_str)
+
+    return board
+
+
+def write_board_state(board):
+    #board in form [[x, y, z, ...], [...], ...] where 0 = empty, 1 = white pawn etc.
+
+    board_dict = {"board" : str(board)}
+    json_str = json.dumps(board_dict)
+
+    with open("interface.json", "w") as file:
+        file.write(json_str)
