437369ef8932cbafca7c871ddaf2395364dcb711
Ben-Edwards44 <benjedwards007@icloud.com>
2024-01-11 16:55:00 +0000

    Fixed bug with file writing

--- src/api/api.go
+++ src/api/api.go
@@ -2,226 +2,213 @@ package api
 
 
 import (
-	"fmt"
 	"os"
-    "strings"
-    "strconv"
+	"strconv"
+	"strings"
 )
 
 
 func panicErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 
 func strToList(str string) [8][8]int {
-    //convert the string "[[1, 2], [3, 4]]" to array [[1, 2], [3, 4]]
+	//convert the string "[[1, 2], [3, 4]]" to array [[1, 2], [3, 4]]
 
-    //remove the [[ and ]] at end
-    str = str[2 : len(str) - 2]
+	//remove the [[ and ]] at end
+	str = str[2 : len(str) - 2]
 
-    strLs := strings.Split(str, "], [")
+	strLs := strings.Split(str, "], [")
 
-    var outList [8][8]int
-    for i := 0; i < 8; i++ {
-        nums := strings.Split(strLs[i], ", ")
+	var outList [8][8]int
+	for i := 0; i < 8; i++ {
+		nums := strings.Split(strLs[i], ", ")
 
-        for x := 0; x < 8; x++ {
-            num, err := strconv.Atoi(nums[x])
+		for x := 0; x < 8; x++ {
+			num, err := strconv.Atoi(nums[x])
 
-            panicErr(err)
+			panicErr(err)
 
-            outList[i][x] = num
-        }
-    }
+			outList[i][x] = num
+		}
+	}
 
-    return outList
+	return outList
 }
 
 
 func removeFirstLast(str string) string {
-    newStr := str[1 : len(str) - 1]
+	newStr := str[1 : len(str) - 1]
 
-    return newStr
+	return newStr
 }
 
 
 func reverseString(str string) string {
-    var chars []string
+	var chars []string
 
-    for i := len(str) - 1; i >= 0; i-- {
-        char := string(str[i])
-        chars = append(chars, char)
-    }
+	for i := len(str) - 1; i >= 0; i-- {
+		char := string(str[i])
+		chars = append(chars, char)
+	}
 
-    reversed := strings.Join(chars, "")
+	reversed := strings.Join(chars, "")
 
-    return reversed
+	return reversed
 }
 
 
 func splitJson(str string) []string {
-    //str in form '"..." : [[...], ...], "..." : "..."' - assumes that every k/v is a string
+	//str in form '"..." : [[...], ...], "..." : "..."' - assumes that every k/v is a string
 
-    var colonInxs []int
+	var colonInxs []int
 
-    for i, x := range str {
-        if x == ':' {
-            colonInxs = append(colonInxs, i)
-        }
-    }
+	for i, x := range str {
+		if x == ':' {
+			colonInxs = append(colonInxs, i)
+		}
+	}
 
-    var splitted []string
-    for _, inx := range colonInxs {
-        //work forwards/back until 2nd double quote
+	var splitted []string
+	for _, inx := range colonInxs {
+		//work forwards/back until 2nd double quote
 
-        key := ""
-        backInx := inx - 1
-        numQuote := 0
+		key := ""
+		backInx := inx - 1
+		numQuote := 0
 
-        for backInx >= 0 && numQuote < 2 {
-            key += string(str[backInx])
+		for backInx >= 0 && numQuote < 2 {
+			key += string(str[backInx])
 
-            if str[backInx] == '"' {
-                numQuote++
-            }
+			if str[backInx] == '"' {
+				numQuote++
+			}
 
-            backInx--
-        }
+			backInx--
+		}
 
-        value := ""
-        //add 2 becuase there is a space after colon
-        forInx := inx + 2
-        numQuote = 0
+		value := ""
+		//add 2 becuase there is a space after colon
+		forInx := inx + 2
+		numQuote = 0
 
-        for forInx < len(str) && numQuote < 2 {
-            value += string(str[forInx])
+		for forInx < len(str) && numQuote < 2 {
+			value += string(str[forInx])
 
-            if str[forInx] == '"' {
-                numQuote++
-            }
+			if str[forInx] == '"' {
+				numQuote++
+			}
 
-            forInx++
-        }
+			forInx++
+		}
 
-        //because the chars were added last to first (but not with key)
-        key = reverseString(key)
+		//because the chars were added last to first (but not with key)
+		key = reverseString(key)
 
-        splitted = append(splitted, key)
-        splitted = append(splitted, value)
-    }
+		splitted = append(splitted, key)
+		splitted = append(splitted, value)
+	}
 
-    return splitted
+	return splitted
 }
 
 
 func jsonLoad(str string) map[string]string {
-    //str will look like {"board" : [[...], ...], "..." : "..."} (no need for nested {})
+	//str will look like {"board" : [[...], ...], "..." : "..."} (no need for nested {})
 
-    //remove {}
-    str = removeFirstLast(str)
+	//remove {}
+	str = removeFirstLast(str)
 
-    kvPairs := splitJson(str)
-    json := make(map[string]string)
+	kvPairs := splitJson(str)
+	json := make(map[string]string)
 
-    fmt.Println(kvPairs)
+	for i := 0; i < len(kvPairs); i += 2 {
+		k := kvPairs[i]
+		v := kvPairs[i + 1]
 
-    for i := 0; i < len(kvPairs); i += 2 {
-        k := kvPairs[i]
-        v := kvPairs[i + 1]
+		//remove ""
+		key := removeFirstLast(k)
+		value := removeFirstLast(v)
 
-        //remove ""
-        key := removeFirstLast(k)
-        value := removeFirstLast(v)
+		json[key] = value
+	}
 
-        json[key] = value
-    }
-
-    return json
+	return json
 }
 
 
 func stateToString(boardState [8][8]int) string {
-    str := "\"["
-    for i, line := range boardState {
-        str += "["
+	str := "\"["
+	for i, line := range boardState {
+		str += "["
 
-        for i, num := range line {
-            str += strconv.Itoa(num)
+		for i, num := range line {
+			str += strconv.Itoa(num)
 
-            if i < len(line) - 1 {
-                str += ", "
-            }
-        }
+			if i < len(line) - 1 {
+				str += ", "
+			}
+		}
 
-        str += "]"
+		str += "]"
 
-        if i < len(boardState) - 1 {
-            str += ", "
-        }
-    }
+		if i < len(boardState) - 1 {
+			str += ", "
+		}
+	}
 
-    //add final ]" for 2d array
-    str += "]\""
+	//add final ]" for 2d array
+	str += "]\""
 
-    return str
+	return str
 }
 
 
 func LoadBoardState() [8][8]int {
-    file, err := os.Open("src/api/interface.json")
+	file, err := os.Open("src/api/interface.json")
 
-    panicErr(err)
+	panicErr(err)
 
-    defer file.Close()
+	defer file.Close()
 
-    buffer := make([]byte, 1024)
+	buffer := make([]byte, 1024)
 
-    //keep reading bytes until there are none left to read
-    for {
-        readBytes, err := file.Read(buffer)
+	//keep reading bytes until there are none left to read
+	for {
+		readBytes, err := file.Read(buffer)
 
-        if readBytes == 0 {
-            break
-        } else {
-            panicErr(err)
-        }
-    }
+		if readBytes == 0 {
+			break
+		} else {
+			panicErr(err)
+		}
+	}
 
-    str := string(buffer)
+	str := string(buffer)
 
-    json := jsonLoad(str)
-    board := strToList(json["board"])
+	json := jsonLoad(str)
+	board := strToList(json["board"])
 
-    return board
+	return board
 }
 
 
 func WriteBoardState(boardState [8][8]int) {
-    str := stateToString(boardState)
+	str := stateToString(boardState)
 
-    //convert to json form
-    writeStr := "{\"board\" : " + str + "}"
-    writeData := []byte(writeStr)
+	//convert to json form
+	writeStr := "{\"board\": " + str + "}"
+	writeData := []byte(writeStr)
 
-    fmt.Println(writeStr)
+	//open file in read/write mode and overwrite existing contents
+	file, err := os.Create("src/api/interface.json")
+	panicErr(err)
 
-    //open file in read/write mode and overwrite existing contents
-    file, err := os.OpenFile("src/api/interface.json", os.O_CREATE, 0644)
-    panicErr(err)
+	defer file.Close()
 
-    defer file.Close()
-
-    _, err = file.Write(writeData)
-    panicErr(err)
-}
-
-
-func Test() {
-    state := LoadBoardState()
-    fmt.Println(state)
-    state[0][5] = 17
-    WriteBoardState(state)
+	_, err = file.Write(writeData)
+	panicErr(err)
 }
