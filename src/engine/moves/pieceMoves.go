0d3dd7d178e8734fb895b5e464a74684c58a2ff3
Ben-Edwards44 <benjedwards007@icloud.com>
2024-01-13 09:05:26 +0000

    Added bishop moves

--- src/engine/moves/pieceMoves.go
+++ src/engine/moves/pieceMoves.go
@@ -2,11 +2,11 @@ package moves
 
 
 //array matching piece values to their appropriate move functions
-var moveFunctions [7]func([64]int, int, int, int) []move = [7]func([64]int, int, int, int) []move{emptyMove, emptyMove, emptyMove, emptyMove, rookMoves, emptyMove, emptyMove}
+var moveFunctions [7]func([64]int, int, int, int) []move = [7]func([64]int, int, int, int) []move{emptyMove, emptyMove, emptyMove, bishopMoves, rookMoves, emptyMove, emptyMove}
 
 
-var dists [8 * 8 * 4]int
-func InitPrecalculate(edgeDists [256]int) {
+var dists [512]int
+func InitPrecalculate(edgeDists [512]int) {
 	dists = edgeDists
 }
 
@@ -30,10 +30,11 @@ func canMove(board [64]int, x int, y int, pieceValue int) (bool, bool) {
 
 
 func rookMoves(board [64]int, x int, y int, pieceValue int) []move {
-	dirInx := x * 32 + y * 4
+	dirInx := x * 64 + y * 8
 
 	var moves []move
 	for dir := 0; dir < 4; dir++ {
+		//get from precalculated
 		edgeDist := dists[dirInx + dir]
 
 		for offset := 1; offset <= edgeDist; offset++ {
@@ -49,6 +50,48 @@ func rookMoves(board [64]int, x int, y int, pieceValue int) []move {
 			} else if dir == 3 {
 				newY = y + offset
 			}
+
+			goodSq, capture := canMove(board, newX, newY, pieceValue) 
+
+			if goodSq {
+				m := move{x, y, newX, newY, pieceValue}
+				moves = append(moves, m)
+			}
+			if capture {
+				break
+			}
+		}
+	}
+
+	return moves
+}
+
+
+func bishopMoves(board [64]int, x int, y int, pieceValue int) []move {
+	dirInx := x * 64 + y * 8
+
+	var moves []move
+	for dir := 0; dir < 4; dir++ {
+		//get from precalculated (+4 since we are looking at diagonal)
+		edgeDist := dists[dirInx + dir + 4]
+
+		for offset := 1; offset <= edgeDist; offset++ {
+			var xMult int
+			var yMult int
+
+			if dir < 2 {
+				xMult = -1
+			} else {
+				xMult = 1
+			}
+			if dir % 2 == 0 {
+				yMult = -1
+			} else {
+				yMult = 1
+			}
+
+			newX := x + offset * xMult
+			newY := y + offset * yMult
 
 			goodSq, capture := canMove(board, newX, newY, pieceValue) 
 
--- src/engine/precalculate.go
+++ src/engine/precalculate.go
@@ -4,9 +4,9 @@ package engine
 import "chess-engine/src/engine/moves"
 
 
-func findDistToEdge() [256]int {
+func findDistToEdge() [512]int {
 	//1d array is faster than 2d
-	var dists [8 * 8 * 4]int
+	var dists [8 * 8 * 8]int
 
 	for x := 0; x < 8; x++ {
 		for y := 0; y < 8; y++ {
@@ -15,12 +15,16 @@ func findDistToEdge() [256]int {
 			left := y
 			right := 7 - y
 
-			inx := x * 32 + y * 4
+			inx := x * 64 + y * 8
 
 			dists[inx] = up
 			dists[inx + 1] = down
 			dists[inx + 2] = left
 			dists[inx + 3] = right
+			dists[inx + 4] = min(up, left)
+			dists[inx + 5] = min(up, right)
+			dists[inx + 6] = min(down, left)
+			dists[inx + 7] = min(down, right)
 		}
 	}
 
