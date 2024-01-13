c62fc8e9dd5ae4cc84dec379d25f290ba21ab880
Ben-Edwards44 <benjedwards007@icloud.com>
2024-01-13 09:36:08 +0000

    Added array for faster x, y multipliers

--- src/engine/moves/pieceMoves.go
+++ src/engine/moves/pieceMoves.go
@@ -4,6 +4,10 @@ package moves
 //array matching piece values to their appropriate move functions
 var moveFunctions [7]func([64]int, int, int, int) []move = [7]func([64]int, int, int, int) []move{emptyMove, emptyMove, emptyMove, bishopMoves, rookMoves, kingMoves, queenMoves}
 
+//array matching distance index to their x, y multipliers
+var xMults [8]int = [8]int{-1, 1, 0, 0, -1, -1, 1, 1}
+var yMults [8]int = [8]int{0, 0, -1, 1, -1, 1, -1, 1}
+
 
 var dists [512]int
 func InitPrecalculate(edgeDists [512]int) {
@@ -38,18 +42,8 @@ func rookMoves(board [64]int, x int, y int, pieceValue int) []move {
 		edgeDist := dists[dirInx + dir]
 
 		for offset := 1; offset <= edgeDist; offset++ {
-			newX := x
-			newY := y
-
-			if dir == 0 {
-				newX = x - offset
-			} else if dir == 1 {
-				newX = x + offset
-			} else if dir == 2 {
-				newY = y - offset
-			} else if dir == 3 {
-				newY = y + offset
-			}
+			newX := x + offset * xMults[dir]
+			newY := y + offset * yMults[dir]
 
 			goodSq, capture := canMove(board, newX, newY, pieceValue) 
 
@@ -76,22 +70,8 @@ func bishopMoves(board [64]int, x int, y int, pieceValue int) []move {
 		edgeDist := dists[dirInx + dir + 4]
 
 		for offset := 1; offset <= edgeDist; offset++ {
-			var xMult int
-			var yMult int
-
-			if dir < 2 {
-				xMult = -1
-			} else {
-				xMult = 1
-			}
-			if dir % 2 == 0 {
-				yMult = -1
-			} else {
-				yMult = 1
-			}
-
-			newX := x + offset * xMult
-			newY := y + offset * yMult
+			newX := x + offset * xMults[dir + 4]
+			newY := y + offset * yMults[dir + 4]
 
 			goodSq, capture := canMove(board, newX, newY, pieceValue) 
 
@@ -122,30 +102,12 @@ func kingMoves(board [64]int, x int, y int, pieceValue int) []move {
 	edgeInx := x * 64 + y * 8
 
 	var moves []move
-	for i := 0; i < 8; i++ {
-		edgeDist := dists[edgeInx + i]
+	for dir := 0; dir < 8; dir++ {
+		edgeDist := dists[edgeInx + dir]
 
 		if edgeDist > 0 {
-			var xStep int
-			var yStep int
-
-			if i == 0 || i == 4 || i == 5 {
-				xStep = -1
-			} else if i == 2 || i == 3 {
-				xStep = 0
-			} else {
-				xStep = 1
-			}
-			if i == 2 || i == 4 || i == 6 {
-				yStep = -1
-			} else if i <= 1 {
-				yStep = 0
-			} else {
-				yStep = 1
-			}
-
-			newX := x + xStep
-			newY := y + yStep
+			newX := x + xMults[dir]
+			newY := y + yMults[dir]
 			
 			good, _ := canMove(board, newX, newY, pieceValue)
 
