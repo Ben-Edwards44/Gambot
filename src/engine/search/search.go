31a1b03cfbd54241ae7051f9cfb6e6a1794eabc0
Ben-Edwards44 <benjedwards007@icloud.com>
2024-02-17 10:34:51 +0000

    Fixed bug with checkmates

--- src/engine/search/search.go
+++ src/engine/search/search.go
@@ -7,7 +7,8 @@ import (
 )
 
 
-const INF int = 100000
+const INF int = 9999999
+const MATESCORE int = 100000
 
 var searchAbandoned bool
 
@@ -49,7 +50,7 @@ func orderMoves(state *moves.GameState, moveList []moves.Move) {
 }
 
 
-func checkWin(state *moves.GameState, isWhite bool, depth int) int {
+func checkWin(state *moves.GameState, isWhite bool) int {
 	//check who has won, or if it is a draw - assumes that the player has no legal moves
 
 	kingPos := state.BlackPiecePos[4][0]
@@ -64,16 +65,10 @@ func checkWin(state *moves.GameState, isWhite bool, depth int) int {
 	inCheck := (kingPosBB & state.NoKingMoveBitBoard) != 0
 
 	if inCheck {
-		if isWhite {
-			//white is checkmated
-			return -INF - depth
-		} else {
-			//black is checkmated
-			return INF + depth
-		}
+		//we are checkmated :(
+		return -MATESCORE  //negative because being checkmated is bad
 	} else {
-		//draw
-		return 0
+		return 0  //draw
 	}
 }
 
@@ -87,10 +82,13 @@ func negamax(state *moves.GameState, isWhite bool, depth int, alpha int, beta in
 
 	startTime := time.Now()
 	
-	if depth == 0 {return quiescenceSearch(state, isWhite, alpha, beta), moves.Move{}}
+	if depth == 0 {return quiescenceSearch(state, isWhite, alpha, beta, timeLeft), moves.Move{}}
 
 	moveList := moves.GenerateAllMoves(state, false)
 	orderMoves(state, moveList)
+
+	//TODO: draws by repetition and 50 move rule etc.
+	if len(moveList) == 0 {return checkWin(state, isWhite), moves.Move{}}  //deal with checkmates and draws
 
 	bestScore := -INF
 	allocatedBestMove := false
@@ -111,7 +109,7 @@ func negamax(state *moves.GameState, isWhite bool, depth int, alpha int, beta in
 			allocatedBestMove = true
 		}
 
-		if score >= beta {return beta, moves.Move{}}
+		if score >= beta {break}  //prune
 
 		if score > alpha {alpha = score}
 	}
@@ -121,58 +119,36 @@ func negamax(state *moves.GameState, isWhite bool, depth int, alpha int, beta in
 
 
 
-func quiescenceSearch(state *moves.GameState, isWhite bool, alpha int, beta int) int {
-	//this does not work :(
+func quiescenceSearch(state *moves.GameState, isWhite bool, alpha int, beta int, timeLeft time.Duration) int {
+	//IMPORTANT: this will not work with checkmates because eval does not return the checkmate values
+	if timeLeft < 0 {
+		searchAbandoned = true
+		return 0
+	}
+	
+	startTime := time.Now()
+
 	staticEval := eval(state, isWhite)
 
-	return staticEval
+	if staticEval >= beta {return beta}  //prune
+	if staticEval > alpha {alpha = staticEval}
+	
+	moveList := moves.GenerateAllMoves(state, true)
+	orderMoves(state, moveList)
 
-	//IMPORTANT: this will not work with checkmates becauseeval does ont return the checkmate values
-
-	if isWhite {
-		//max player
-		if staticEval > alpha {
-			alpha = staticEval
-		}
-	} else {
-		//min player
-		if staticEval < beta {
-			beta = staticEval
-		}
-	}
-
-	if staticEval >= beta {
-		return beta
-	} //prune
-
-	captMoves := moves.GenerateAllMoves(state, true)
-
-	if len(captMoves) == 0 {return staticEval}  //quiet position reached - return the evalutation
-
-	bestScore := INF + 1  //+1 so that engine will still move if in forced mate
-	if isWhite {bestScore = -INF - 1}  //-1 so that engine will still move if in forced mate
-
-	for _, i := range captMoves {
+	for _, i := range moveList {
 		moves.MakeMove(state, i)
-		score := quiescenceSearch(state, !isWhite, alpha, beta)
+
+		elapsed := time.Since(startTime)
+		score := -quiescenceSearch(state, !isWhite, -beta, -alpha, timeLeft - elapsed)
+	
 		moves.UnMakeLastMove(state)
 
-		if isWhite {
-			//max player
-			if score > bestScore {bestScore = score}
-
-			if score > alpha {alpha = score}
-		} else {
-			//min player
-			if score < bestScore {bestScore = score}
-
-			if score < beta {beta = score}
-		}
-
-		if beta <= alpha {break}  //prune position (opponent already has a better position)
+		if score >= beta {return beta}  //prune
+		if score > alpha {alpha = score}
 	}
 
-	return bestScore
+	return alpha
 }
 
 
@@ -180,7 +156,7 @@ func GetBestMove(state *moves.GameState) moves.Move {
 	searchAbandoned = false
 
 	startTime := time.Now()
-	timeLeft := time.Duration(time.Millisecond * 1000)
+	timeLeft := time.Duration(time.Millisecond * 500)
 
 	depth := 1
 	var bestMove moves.Move
@@ -189,7 +165,7 @@ func GetBestMove(state *moves.GameState) moves.Move {
 		elapsed = time.Since(startTime)
 		timeLeft -= elapsed
 
-		_, searchBestMove := negamax(state, state.WhiteToMove, depth, -INF, INF, timeLeft)
+		score, searchBestMove := negamax(state, state.WhiteToMove, depth, -INF, INF, timeLeft)  //NOTE: don't need to -score because this call is from the POV of the engine
 		
 		if !searchAbandoned {
 			bestMove = searchBestMove
@@ -197,7 +173,13 @@ func GetBestMove(state *moves.GameState) moves.Move {
 			break
 		}
 
+		if score == MATESCORE {break}  //we have found a mate, so don't search any deeper
+
 		depth++
+	}
+
+	if (bestMove == moves.Move{}) {
+		panic("Best move is an empty move")
 	}
 
 	return bestMove
