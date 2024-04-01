902f8d77734ccbbf2ef14d07a76444be4469d355
Ben-Edwards44 <benjedwards007@icloud.com>
2024-04-01 19:44:12 +0100

    Removed uneeded comment

--- gambot/src/engine/search/search.go
+++ gambot/src/engine/search/search.go
@@ -228,7 +228,3 @@ func GetBestMove(state *board.GameState, moveTime int) *moves.Move {
 
 	return bestMove  //NOTE: if no moves are available (in mate), this will be an empty move
 }
-
-
-//6k1/p4pp1/2p1pn2/8/6p1/2p5/r7/1r1N1K2 b - - 1 56
-//position fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" moves e2e4 b8c6 d2d4 g8f6 e4e5 f6d5 c2c4 d5b6 c4c5 b6d5 f1c4 e7e6 b1c3 d5c3 b2c3 d7d5 c4b5 c8d7 d1f3 c6e5 b5d7 e5d7 c5c6 b7c6 c3c4 f8b4 e1f1 e8g8 f3e2 b4c3 c1b2 c3b2 e2b2 d5c4 a1c1 d7b6 h2h4 a8b8 h1h3 b6d5 b2a1 d5f4 h3h2 d8d5 g2g3 f4d3 c1d1 b8d8 g1e2 f8e8 e2c3 d5d4 c3e2 d4d5 a1b1 c6c5 e2f4 d5f3 f4h3 d8b8 b1c2 b8b2 c2a4 c7c6 f1g1 h7h6 d1f1 c4c3 a4a5 c3c2 a5c3 f3d1 f1d1 c2d1q g1g2 d1e2 h2h1 d3f2 h1e1 f2d1 e1e2 b2e2 g2f3 d1c3 a2a3 e2a2 h4h5 a2a3 h3f2 c3d5 f3g2 d5f6 f2d1 f6h5 g3g4 h5f6 d1f2 c5c4 g4g5 h6g5 f2d1 c4c3 d1e3 a3a2 g2f1 e8b8 e3d1 b8b1 f1e1 g5g4 e1f1 g4g3 f1e1 a2b2 e1f1 g3g2 f1g1 b1d1 g1h2 g2g1q h2h3 b2b3 h3h4 g1h2 h4g5 f6d7 g5g4 h2g1 g4h5 b3b5 h5h4 b5a5 h4h3 a5b5 h3h4 b5a5
